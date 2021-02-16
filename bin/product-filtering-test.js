c5b8793d301ba88fbd1da91305559ddbe894edb8
Falk Hoppe <falkhoppe81@gmail.com>
2021-02-16 18:27:08 +0100

    remove dev https setup

--- bin/product-filtering-test.js
+++ bin/product-filtering-test.js
@@ -1,28 +1,11 @@
 #!/usr/bin/env node
 
-import { readFileSync } from "fs";
-import { createServer } from "https";
-import app from "../index.js";
+import app from '../index.js'
 
 const config = {
-  port: process.env.PORT,
-  ssl: {
-    key: process.env.SSL_KEY_PATH,
-    cert: process.env.SSL_CERT_PATH
-  }
-};
+  port: process.env.PORT
+}
 
-const listener =
-  config.ssl.key && config.ssl.cert
-    ? createServer(
-        {
-          key: readFileSync(config.ssl.key),
-          cert: readFileSync(config.ssl.cert)
-        },
-        app(config)
-      )
-    : app(config);
-
-const server = listener.listen(config.port, () => {
-  console.log(`listening to port: ${server.address().port}`);
-});
+const server = app(config).listen(config.port, () => {
+  console.log(`listening to port: ${server.address().port}`)
+})
