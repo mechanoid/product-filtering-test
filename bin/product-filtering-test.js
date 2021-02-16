6e8d0dd2057dba6ae49cbca16e92a88c2b4c86b6
Falk Hoppe <falkhoppe81@gmail.com>
2021-02-16 18:26:27 +0100

    initialize express app

--- bin/product-filtering-test.js
+++ bin/product-filtering-test.js
@@ -1,1 +1,28 @@
 #!/usr/bin/env node
+
+import { readFileSync } from "fs";
+import { createServer } from "https";
+import app from "../index.js";
+
+const config = {
+  port: process.env.PORT,
+  ssl: {
+    key: process.env.SSL_KEY_PATH,
+    cert: process.env.SSL_CERT_PATH
+  }
+};
+
+const listener =
+  config.ssl.key && config.ssl.cert
+    ? createServer(
+        {
+          key: readFileSync(config.ssl.key),
+          cert: readFileSync(config.ssl.cert)
+        },
+        app(config)
+      )
+    : app(config);
+
+const server = listener.listen(config.port, () => {
+  console.log(`listening to port: ${server.address().port}`);
+});
---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+const app = express()
+app.use(helmet())
+app.use(morgan('combined'))
+
+export default config => {
+  console.log(config)
+  return app
+}
